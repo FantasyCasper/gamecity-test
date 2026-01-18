39e7eb18c7f77cfdbe18936781d2b55f87c20760
FantasyCasper <fantasycasper9@gmail.com>
2026-01-18 11:36:03 +0100

    UPdate

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    =============================== */
 
 // De naam is veranderd om de cache te forceren
-const CACHE_NAAM = 'checklist-app-cache-v0.110';
+const CACHE_NAAM = 'checklist-app-cache-v0.110.1';
 
 // De lijst is bijgewerkt met de nieuwe map
 const urlsToCache = [
